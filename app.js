be2036412adbb888ac63b1368f986d84d22a0376
itwadesh <itswadesh@gmail.com>
2016-02-09 08:57:18 +0530

    Multiple file upload implemented into server

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var path = require('path');
+var http = require('http');
+var multiparty = require('connect-multiparty');
+var multipartyMiddleware = multiparty();
+
+app.use(multiparty({
+    uploadDir: './uploads'
+}));
+
+app.post('/upload', multipartyMiddleware, function(req, resp) {
+  console.log(req.body, req.files);
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('port', process.env.PORT || 3000);
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
